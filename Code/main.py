ad397f3453ab76b12d270b77d387fa00c7525e7a
VoidAsMad <103942316+VoidAsMad@users.noreply.github.com>
2022-05-17 15:25:09 +0900

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -2,6 +2,7 @@
 import discord
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
+from def import *
 
 bot = commands.Bot(command_prefix=['?'], intents=discord.Intents.all())
 slash = SlashCommand(bot, sync_commands=True)
